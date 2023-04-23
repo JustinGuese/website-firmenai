ea42c6ef1a736592184a132bb97c1c203ccf94f1
Justin Güse <jguese@Justins-MacBook-Pro.local>
2023-04-23 21:42:33 +0200

    new build instructs

--- buildPush.sh
+++ buildPush.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 staticjinja build --outpath=./docs
-docker build -t guestros/firmenai:latest .
+docker build -t guestros/firmenai:latest --platform linux/amd64 .
 docker push guestros/firmenai:latest
