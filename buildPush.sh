93f65ccfd1634bd3af7458e389e949caa454b49c
Justin Güse <guese.justin@gmail.com>
2023-04-24 21:30:20 +0200

    german subpage

--- buildPush.sh
+++ buildPush.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 staticjinja build --outpath=./docs
+staticjinja build --outpath=./docs/de --srcpath=./german/templates
 docker build -t guestros/firmenai:latest --platform linux/amd64 .
 docker push guestros/firmenai:latest
