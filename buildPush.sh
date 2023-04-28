a081ed743eb6135436d9b583893da04abb28c3b5
Justin Güse <guese.justin@gmail.com>
2023-04-28 13:28:45 +0200

    own site 4 each lang

--- buildPush.sh
+++ buildPush.sh
@@ -3,3 +3,6 @@ staticjinja build --outpath=./docs
 staticjinja build --outpath=./docs/de --srcpath=./german/templates
 docker build -t guestros/firmenai:latest --platform linux/amd64 .
 docker push guestros/firmenai:latest
+# then german
+docker build -t guestros/firmenai:latest-german --platform linux/amd64 . -f Dockerfile.de
+docker push guestros/firmenai:latest-german
